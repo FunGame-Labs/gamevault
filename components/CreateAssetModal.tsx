1600c95c38df6a7037c781a2ab76a5c7bceffd36
aeither <36173828+aeither@users.noreply.github.com>
2023-05-15 21:27:44 +0200

    create asset

--- components/CreateAssetModal.tsx
+++ components/CreateAssetModal.tsx
@@ -10,36 +10,109 @@ import {
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useDB } from '@/hooks/use-db'
+import { upload } from '@spheron/browser-upload'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { Textarea } from './ui/textarea'
 
+type FormValues = {
+  name: string
+  description: string
+  files: FileList
+}
+
 export function CreateAssetModal() {
+  const [open, setOpen] = useState(false)
+  const { saveFile } = useDB()
+
+  const { register, handleSubmit } = useForm<FormValues>()
+
+  const uploadFile = async (files: File[]) => {
+    const response = await fetch(`/api/token`) // get the temporary access token from server
+    const resJson = await response.json()
+    const token = resJson.uploadToken
+
+    let currentlyUploaded = 0
+
+    const { uploadId, bucketId, protocolLink, dynamicLinks } = await upload(files, {
+      token,
+      onChunkUploaded: (uploadedSize, totalSize) => {
+        currentlyUploaded += uploadedSize
+        console.log(`Uploaded ${currentlyUploaded} of ${totalSize} Bytes.`)
+      },
+    })
+    console.log(
+      '🚀 ~ file: upload.tsx:28 ~ uploadFile ~ uploadId, bucketId, protocolLink, dynamicLinks:',
+      uploadId,
+      bucketId,
+      protocolLink,
+      dynamicLinks
+    )
+    return protocolLink
+  }
+
+  const onSubmit = async (data: FormValues) => {
+    console.log('🚀 ~ file: upload.tsx:31 ~ onSubmit ~ data:', data)
+    const fileListArray = Array.from(data.files)
+
+    const link = await uploadFile(fileListArray)
+    await saveFile({ title: data.name, description: data.description, file: link })
+
+    setOpen(false)
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button variant="outline">Create Asset</Button>
       </DialogTrigger>
+
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create Asset</DialogTitle>
-          <DialogDescription>Upload your asset here</DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Title
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogHeader>
+            <DialogTitle>Create Asset</DialogTitle>
+            <DialogDescription>Upload your asset here</DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Title
+              </Label>
+              <Input
+                id="name"
+                value="Magic Potions"
+                className="col-span-3"
+                {...register('name')}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Textarea
+                id="description"
+                value="a powerful artifact that has been passed down through generations of wizards and sorcerers. This ancient talisman is said to hold within it the secrets of magic and the ability to harness its power."
+                className="col-span-3"
+                {...register('description')}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="files" className="text-right">
+                Assets
+              </Label>
+              <Input
+                type="file"
+                id="files"
+                className="col-span-3"
+                {...register('files')}
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Description
-            </Label>
-            <Textarea id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
